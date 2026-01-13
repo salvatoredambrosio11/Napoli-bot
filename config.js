dc64405606481ce1f94f5b91891d3d5728b63fbb
salvatoredambrosio11 <salvatoredambrosio671@gmail.com>
2026-01-13 19:41:54 +0100

    Update config.js
    Napoli bot

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
 global.owner = [
-  ['393773842461', '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', true],
-  ['xxxxxxxxxx'], //mettete il vostro numero al posto delle x e copiate sopra il formato dopo ovvero 'nome', true
+  ['+393336413960', 'SSC NAPOLI', true],
+  ['+1(918)402-5124'], //mettete il vostro numero al posto delle x e copiate sopra il formato dopo ovvero 'nome', true
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
