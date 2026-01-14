2f418e788c820aae18b2e57e1052bf4ee9dc8755
salvatoredambrosio11 <salvatoredambrosio671@gmail.com>
2026-01-14 09:42:39 +0100

    Update config.js
    Napoli bot

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 global.owner = [
   ['393336413960', 'Salvatore', true],
   ['19184025124', 'Simone', true],
-  ['××××××××××'],
+  ['393510516821','business',true],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx']
